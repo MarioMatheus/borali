e37a704c22ceb3a65f32ce327f32249654b852d9
Mario Matheus <mariomatheu@gmail.com>
2017-11-06 11:36:16 -0300

    Modelando página de adição de pontos

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 
 //To use of camera and storage in firebase
 import { storage, initializeApp } from 'firebase';
 //import { FIREBASE_CONFIG } from '../../app/firebase.config';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+
+import { Point } from '../../models/point';
 
 
 @Component({
@@ -14,9 +16,11 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 
 export class PostPage {
 
-  private image: any;
+  private _image: any;
+  private _titlePoint: String;
+  private _description: String;
 
-  constructor(public camera: Camera, public navCtrl: NavController) {
+  constructor(public camera: Camera, public navCtrl: NavController, public loadingCtrl: LoadingController) {
       //initializeApp(FIREBASE_CONFIG);
   }
 
@@ -35,16 +39,32 @@ export class PostPage {
       }
 
       const result = await this.camera.getPicture(options);
+
+      let loading = this.loadingCtrl.create({
+        content: 'Carregando foto...'
+      });
+      loading.present();
+
       const image = `data:image/jpeg;base64,${result}`;
       const pictures = storage().ref('pictures/myPhoto');
-      pictures.putString(image, 'data_url');
+      pictures.putString(image, 'data_url')
+      .then(savepic => {
+        this._image = savepic.downloadURL;
+      });
 
-      this.image = pictures.getDownloadURL(); 
+      loading.dismiss();
+
     }
     catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
+  }
+
+  savePoint() {
+    let newPoint = new Point(this._image, this._titlePoint);
+    newPoint.setDescription(this._description);
+    // firebase.push(newPoint);
   }
 
 }
