0f609666312e219a9fed77f5fdcf17912077c9f2
Gabriel Palhares <gaapalhares@gmail.com>
2017-11-17 13:25:12 -0300

    confirmação de senha no registro

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -13,17 +13,33 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class RegisterPage {
 
   
-  private user : User;
+  private user          : User;
+  public firstPassword  : string;
+  public seconPassword  : string;
   
   constructor(private afAuth: AngularFireAuth, 
     public navCtrl: NavController, public navParams: NavParams) {
     this.user = new User();
   }
 
+  public verifyPassword(): boolean{
+    if(this.firstPassword == this.seconPassword){
+      this.user._password = this.firstPassword;
+      return true
+    }
+    else{
+      console.log('Divergência na senha')
+      return false
+    }
+  }
+  
   async register(user: User){
     try{
-      const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.user.getEmail(), this.user.getPassword());
-      this.navCtrl.pop();
+      if(this.verifyPassword()){
+        const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.user.getEmail(), this.user.getPassword());
+        this.navCtrl.pop();
+      }
+      
     }
     catch(e){
       console.error(e);
